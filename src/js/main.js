3f21cfe030cd83823303a8c7cbd0699ecd69a4ca
Ssonic8 <gladunalexv@gmail.com>
2018-12-08 18:35:04 +0300

    js+prefixs

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,6 @@ var button = document.querySelector(".main-nav__toggle");
 
 button.addEventListener("click", function(evt){
 	evt.preventDefault();
-	menu.classList.toggle('visually-hidden');
+	menu.classList.toggle('main-nav__wrapper--active');
 	button.classList.toggle('main-nav__toggle--active');
 });
